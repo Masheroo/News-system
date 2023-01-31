c9623d20e459126b07af973245ce154172748934
“mashero” <dima.vuynov@yandex.ru>
2023-01-31 16:42:18 +0300

    Header and main page created

---
+++ resources/js/components/pages/nMain.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="wrapper">
+            <div class="total-popular-wrapper">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
